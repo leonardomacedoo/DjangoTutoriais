32f5fc81930e9043b532d5c0583bd9265012a640
Leonardo Macedo <leojoinville2010@hotmail.com>
2023-10-30 10:11:24 -0300

    Finalização Tutorial_REST 02 e 03

---
+++ Tutorial_REST_02/config/urls.py
@@ -0,0 +1,8 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls')),
+]
